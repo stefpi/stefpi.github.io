7539d5cbf26d05849ed49d0e703beef9c136bc0f
stefpi <stef@ceatanostra.org>
2025-11-19 15:19:54 -0500

    about page update

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,6 @@ export default function Home() {
   const jobs = [
     {
       title: "embedded systems engineer",
-      place: "arboreal systems / neutrino black box",
       description: [
         "making apps for interfacing with power distribution modules for vehicles.",
         "developing application layer protocol for microcontroller communication over CANFD."
@@ -16,24 +15,20 @@ export default function Home() {
     },
     {
       title: "chief technology officer",
-      place: "cordax",
       description: [
         "created novel mechanical, electrical and plumbing (mep) auto-routing schematic design software for residential construction"
       ]
     },
     {
       title: "software developer",
-      place: "distributive",
       description: ["working on distributed computing across edge devices by leverging the browser web stack."]
     },
     {
       title: "backend developer",
-      place: "infera",
       description: ["designing interative and interoperable ai agents running on a distributed LLM inferencing network."]
     },
     {
       title: "chief of web development",
-      place: "qmind",
       description: ["cultivating the look of the digital face of Canada's largest undergraduate artificial intelligence organization."]
     }
   ]
@@ -56,8 +51,8 @@ export default function Home() {
         <p>A site about stuff that I have done so I can remember it.</p>
         <b>
           My name is Stefan. I am actively learning and producing things related to topics in computer science, distributed computing, machine learning, design and electronics.
-          <h6>check out <a href="/blog"><u>/blog</u></a> to see what I have been interested in recently.</h6>
         </b>
+        <h6>check out <a href="/blog"><u>/blog</u></a> to see what I have been interested in recently.</h6>
 
         <hl />
 
@@ -65,7 +60,7 @@ export default function Home() {
         <div className="experience"> 
           <div className="card">
             <div className="cardHeader">
-              <p><u><i>{jobs[0].title}</i> @ <b>{jobs[0].place}</b></u></p>
+              <p><u><i>{jobs[0].title}</i></u></p>
             </div>
             <div className="cardBody">
               {jobs[0].description.map((desc, index) => (
@@ -84,7 +79,7 @@ export default function Home() {
               return (
                 <div className="card" key={ index }>
                   <div className="cardHeader">
-                    <p><u><i>{card.title}</i> @ <b>{card.place}</b></u></p>
+                    <p><u><i>{card.title}</i></u></p>
                   </div>
                   <div className="cardBody">
                     {card.description.map((desc, index) => (
@@ -96,7 +91,7 @@ export default function Home() {
             })
           }
         </div>
-        <h6>check out <a href="/projects"><u>/projects</u></a> to see some of the results of these jobs and some other cool projects</h6>
+        {/* <h6>check out <a href="/projects"><u>/projects</u></a> to see some of the results of these jobs and some other cool projects</h6> */}
 
         <hl />
 
